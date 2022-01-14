d9afa8941b5908158838e5bd3c3844960b7b6a83
Yui Tung Lee <ytleevicky@gmail.com>
2022-01-14 22:50:39 +0800

    Initial Commit

---
+++ BMI-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Vicky Lee on 14/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
